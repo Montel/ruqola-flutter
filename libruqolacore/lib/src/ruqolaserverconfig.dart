8d8c9643cf24ca54cea06839a3a46e547ccf86a0
Laurent Montel <laurent@kdab.com>
2025-02-05 13:21:37 +0100

    Minor

--- libruqolacore/lib/src/ruqolaserverconfig.dart
+++ libruqolacore/lib/src/ruqolaserverconfig.dart
@@ -3,76 +3,79 @@
  *
  * SPDX-License-Identifier: LGPL-2.0-or-later
  */
+import 'package:libruqolacore/src/ruqolaserverconfigpassword.dart';
+
 enum AuthMethodType {
-  unknown(1 << 0),
-  twitter(1 << 1),
-  faceBook(1 << 2),
-  gitHub(1 << 4),
-  gitLab(1 << 8),
-  google(1 << 16),
-  linkedin(1 << 32),
-  wordpress(1 << 64),
-  password(1 << 128),
-  apple(1 << 256),
-  nextCloud(1 << 512),
-  personalAccessToken(1 << 1024);
+  unknown(0),
+  twitter(1 << 0),
+  faceBook(1 << 1),
+  gitHub(1 << 2),
+  gitLab(1 << 4),
+  google(1 << 8),
+  linkedin(1 << 16),
+  wordpress(1 << 32),
+  password(1 << 64),
+  apple(1 << 128),
+  nextCloud(1 << 256),
+  personalAccessToken(1 << 512);
 
   final int value;
   const AuthMethodType(this.value);
 }
 
 class RuqolaServerConfig {
-  /*
   // Server config
-    PasswordSettings mPasswordSettings;
-    QString mUniqueId;
-    QString mJitsiMeetUrl;
-    QString mJitsiMeetPrefix;
-    QString mFileUploadStorageType;
-    QString mSiteUrl;
-    QString mSiteName;
-    QString mServerVersionStr;
-    QString mAutoTranslateGoogleKey;
-    QString mChannelNameValidation;
-    QString mUserNameValidation;
-    QString mAccountsDefaultUserPreferencesDesktopNotifications;
-    QString mAccountsDefaultUserPreferencesPushNotifications;
-    AuthenticationManager::AuthMethodTypes mServerAuthTypes = AuthenticationManager::AuthMethodType::Unknown;
+  RuqolaServerConfigPassword mPasswordSettings = RuqolaServerConfigPassword();
+  String mUniqueId = '';
+  String mJitsiMeetUrl = '';
+  String mJitsiMeetPrefix = '';
+  String mFileUploadStorageType = '';
+  String mSiteUrl = '';
+  String mSiteName = '';
+  String mServerVersionStr = '';
+  String mAutoTranslateGoogleKey = '';
+  String mChannelNameValidation = '';
+  String mUserNameValidation = '';
+  String mAccountsDefaultUserPreferencesDesktopNotifications = '';
+  String mAccountsDefaultUserPreferencesPushNotifications = '';
+  /*
+    int mServerAuthTypes = AuthMethodType.unknown.value;
     AuthenticationManager::AuthMethodTypes mRuqolaAuthMethodTypes = AuthenticationManager::AuthMethodType::Unknown;
-    int mBlockEditingMessageInMinutes = 5;
-    int mBlockDeletingMessageInMinutes = 5;
-    int mServerVersionMajor = -1;
-    int mServerVersionMinor = -1;
-    int mServerVersionPatch = -1;
-    qint64 mFileMaxFileSize = -1;
+    */
+  int mBlockEditingMessageInMinutes = 5;
+  int mBlockDeletingMessageInMinutes = 5;
+  int mServerVersionMajor = -1;
+  int mServerVersionMinor = -1;
+  int mServerVersionPatch = -1;
+  int mFileMaxFileSize = -1;
 
-    bool mMessageAllowConvertLongMessagesToAttachment = false;
-    bool mUIUseRealName = false;
-
-    ServerConfigFeatureTypes mServerConfigFeatureTypes = ServerConfigFeatureType::None;
+  bool mMessageAllowConvertLongMessagesToAttachment = false;
+  bool mUIUseRealName = false;
+/*
+    ServerConfigFeatureTypes mServerConfigFeatureTypes = ServerConfigFeatureType.none;
 
     List<String> mMediaWhiteList;
     List<String> mMediaBlackList;
 
     ConfigWithDefaultValue mLogoUrl;
     ConfigWithDefaultValue mFaviconUrl;
-    int mLoginExpiration = -1;
-    int mMessageMaximumAllowedSize = -1;
-    int mMessageGroupingPeriod = -1;
-    int mDirectMessageMaximumUser = -1;
-    int mMessageQuoteChainLimit = -1;
-    int mEmbedCacheExpirationDays = 30; // Default value in RC
+    */
+  int mLoginExpiration = -1;
+  int mMessageMaximumAllowedSize = -1;
+  int mMessageGroupingPeriod = -1;
+  int mDirectMessageMaximumUser = -1;
+  int mMessageQuoteChainLimit = -1;
+  int mEmbedCacheExpirationDays = 30; // Default value in RC
 
-    bool mHasEnterpriseSupport = false;
-    bool mAccountsAllowInvisibleStatusOption = true; // Default true
-    bool mUserDataDownloadEnabled = true;
-    bool mDeviceManagementEnableLoginEmails = false;
-    bool mDeviceManagementAllowLoginEmailpreference = false;
-    bool mAllowCustomStatusMessage = false;
-    bool mPreviewEmbed = false;
-    bool mAllowEmailNotifications = true; // Default true
-    bool mAllowEmailVerification = false;
-    bool mFederationEnabled = false;
-    bool mAccountsManuallyApproveNewUsers = false;
-*/
+  bool mHasEnterpriseSupport = false;
+  bool mAccountsAllowInvisibleStatusOption = true; // Default true
+  bool mUserDataDownloadEnabled = true;
+  bool mDeviceManagementEnableLoginEmails = false;
+  bool mDeviceManagementAllowLoginEmailpreference = false;
+  bool mAllowCustomStatusMessage = false;
+  bool mPreviewEmbed = false;
+  bool mAllowEmailNotifications = true; // Default true
+  bool mAllowEmailVerification = false;
+  bool mFederationEnabled = false;
+  bool mAccountsManuallyApproveNewUsers = false;
 }
