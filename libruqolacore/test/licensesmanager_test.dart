aaae6a178113b75da86877af05462ef2de39fb4a
Laurent Montel <laurent@kdab.com>
2025-02-14 13:21:47 +0100

    Add support for licenses parsing

--- libruqolacore/test/licensesmanager_test.dart
+++ libruqolacore/test/licensesmanager_test.dart
@@ -6,12 +6,44 @@
 
 import 'package:libruqolacore/libruqolacore.dart' as libruqolacore;
 import 'package:test/test.dart';
+import 'libruqolacoretest.dart';
 
 void main() {
   final licensesmanager = libruqolacore.LicensesManager();
   group('Licenses Info', () {
     test('Test Initial Licenses Info values', () {
       expect(licensesmanager.licenses.isEmpty, true);
+    });
+
+    test('test1 json', () {
+      final data = extractJsonData("licensesmanager", "test1.json");
+
+      final f = libruqolacore.LicensesManager();
+      f.parseLicenses(data);
+      final List<String> licenses = [
+        "auditing",
+        "canned-responses",
+        "ldap-enterprise",
+        "livechat-enterprise",
+        "voip-enterprise",
+        "omnichannel-mobile-enterprise",
+        "engagement-dashboard",
+        "push-privacy",
+        "scalability",
+        "teams-mention",
+        "saml-enterprise",
+        "oauth-enterprise",
+        "device-management",
+        "federation",
+        "videoconference-enterprise",
+        "message-read-receipt",
+        "outlook-calendar",
+        "hide-watermark",
+        "custom-roles",
+        "accessibility-certification"
+      ];
+
+      expect(f.licenses, licenses);
     });
   });
 }
