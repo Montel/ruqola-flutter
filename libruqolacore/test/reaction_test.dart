40f6b8295d49ee3b2afee190d9e5ebbef6e3e2c0
Laurent Montel <laurent@kdab.com>
2025-02-18 22:54:17 +0100

    Add autotests

--- libruqolacore/test/reaction_test.dart
+++ libruqolacore/test/reaction_test.dart
@@ -8,13 +8,31 @@ import 'package:test/test.dart';
 
 void main() {
   group('Reaction', () {
-    /*
-    final reaction = Reaction();
+    test('Test tooltip', () {
+      {
+        final reaction = Reaction(reactionName: ":foo:", userNames: []);
+        expect(reaction.convertedUsersNameAtToolTip(), "");
+      }
 
-    test('Test Initial values', () {
-      expect(reaction.reactionName.isEmpty, true);
-      expect(reaction.userNames.isEmpty, true);
+      {
+        final reaction = Reaction(reactionName: ":foo:", userNames: ["bla"]);
+        expect(
+            reaction.convertedUsersNameAtToolTip(), "bla reacted with :foo:");
+      }
+
+      {
+        final reaction =
+            Reaction(reactionName: ":foo:", userNames: ["bla", "blo"]);
+        expect(reaction.convertedUsersNameAtToolTip(),
+            "bla and blo reacted with :foo:");
+      }
+
+      {
+        final reaction =
+            Reaction(reactionName: ":foo:", userNames: ["bla", "blo", "bli"]);
+        expect(reaction.convertedUsersNameAtToolTip(),
+            "bla, blo and bli reacted with :foo:");
+      }
     });
-    */
   });
 }
