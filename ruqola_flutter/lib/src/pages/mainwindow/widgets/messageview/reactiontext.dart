f031d05a6d9b444a7ce53d8516facb305f1e662a
Laurent Montel <laurent@kdab.com>
2025-02-19 08:37:08 +0100

    Prepare to add own reaction component

---
+++ ruqola_flutter/lib/src/pages/mainwindow/widgets/messageview/reactiontext.dart
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Laurent Montel <laurent.montel@kdab.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+import 'package:flutter/material.dart';
+import 'package:flutter_emoji/flutter_emoji.dart';
+import 'package:libruqolacore/libruqolacore.dart';
+
+class ReactionText extends StatefulWidget {
+  final Reaction reaction;
+  const ReactionText({
+    super.key,
+    required this.reaction,
+  });
+
+  @override
+  ReactionTextState createState() => ReactionTextState();
+}
+
+class ReactionTextState extends State<ReactionText> {
+  @override
+  Widget build(BuildContext context) {
+    return Tooltip(
+        message: widget.reaction.convertedUsersNameAtToolTip(),
+        child: GestureDetector(
+          child: Text(EmojiParser().get(widget.reaction.reactionName).code),
+          onTap: () => {
+            // Remove emoji or add it.
+          },
+        ));
+  }
+}
