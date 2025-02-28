bcc9ad0453b911c84792be0fbe4fe933263766ba
Laurent Montel <laurent@kdab.com>
2025-02-28 19:35:04 +0100

    improve previewcommand

--- libruqolacore/lib/src/commands/previewcommand.dart
+++ libruqolacore/lib/src/commands/previewcommand.dart
@@ -4,16 +4,46 @@
  * SPDX-License-Identifier: LGPL-2.0-or-later
  */
 
+enum TypePreview {
+  unknown,
+  image,
+  video,
+  audio,
+  text,
+  other,
+}
+
+TypePreview typePreviewFromString(String str) {
+  if (str == "image") {
+    return TypePreview.image;
+  } else if (str == "video") {
+    return TypePreview.video;
+  } else if (str == "audio") {
+    return TypePreview.audio;
+  } else if (str == "text") {
+    return TypePreview.text;
+  } else if (str == "other") {
+    return TypePreview.other;
+  } else {
+    print("Unknown type preview: $str");
+  }
+  return TypePreview.unknown;
+}
+
 class PreviewCommand {
   final String id;
   final String value;
-  final String type;
+  final TypePreview type;
 
   PreviewCommand({required this.id, required this.value, required this.type});
 
   factory PreviewCommand.fromJson(Map<String, dynamic> json) {
-    return PreviewCommand(
-        id: json["id"] ?? '', value: json["value"] ?? '', type: json["type"] ?? '');
+    final TypePreview type = typePreviewFromString(json["type"] ?? '');
+    return PreviewCommand(id: json["id"] ?? '', value: json["value"] ?? '', type: type);
+  }
+
+  bool isValid() {
+    return id.isNotEmpty && value.isNotEmpty && type != TypePreview.unknown;
   }
 
   @override
