ab41fc0ac5e3207541d2d3764f36064875c79ebd
Laurent Montel <laurent@kdab.com>
2025-03-27 15:23:26 +0100

    Add completer

--- ruqola_flutter/lib/src/pages/mainwindow/widgets/messageline.dart
+++ ruqola_flutter/lib/src/pages/mainwindow/widgets/messageline.dart
@@ -1,18 +1,13 @@
-/*
- * SPDX-FileCopyrightText: 2024-2025 Laurent Montel <laurent.montel@kdab.com>
- *
- * SPDX-License-Identifier: LGPL-2.0-or-later
- */
-
 import 'package:flutter/material.dart';
 import 'package:libruqolacore/libruqolacore.dart';
 import 'package:ruqola_flutter/src/pages/mainwindow/widgets/sharedvalue.dart';
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
-// Look at https://github.com/Fintasys/emoji_picker_flutter/blob/master/example/lib/main_whatsapp.dart
+import 'package:flutter/services.dart'; // Pour ContextMenuController
 
 class Messageline extends StatefulWidget {
   final Rocketchataccount account;
   const Messageline(this.account, {super.key});
+
   @override
   MessagelineState createState() => MessagelineState();
 }
@@ -29,110 +24,104 @@ class MessagelineState extends State<Messageline> {
     'Builder',
     'Context',
   ];
-  List<String> _filteredSuggestions = [];
 
-  @override
-  void initState() {
-    super.initState();
-    _controller.addListener(_onTextChanged);
+  void insertEmoji(String emoji) {
+    final text = _controller.text;
+    final selection = _controller.selection;
+    final newText = text.replaceRange(
+      selection.start,
+      selection.end,
+      emoji,
+    );
+    final newSelection = TextSelection.collapsed(offset: selection.start + emoji.length);
+    _controller.text = newText;
+    _controller.selection = newSelection;
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  void showEmojiPicker() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return EmojiPicker();
+      },
+    );
   }
-
-  void _onTextChanged() {
-    setState(() {
-      _filteredSuggestions = _suggestions
-          .where((suggestion) => suggestion.toLowerCase().contains(_controller.text.toLowerCase()))
-          .toList();
-    });
-  }
-
-  // _controller.addListener(_onTextChanged);
 
   @override
   Widget build(BuildContext context) {
-    void insertEmoji(String emoji) {
-      final text = _controller.text;
-      final selection = _controller.selection;
-      final newText = text.replaceRange(
-        selection.start,
-        selection.end,
-        emoji,
-      );
-      final newSelection = TextSelection.collapsed(offset: selection.start + emoji.length);
-      _controller.text = newText;
-      _controller.selection = newSelection;
-    }
-
-    void showEmojiPicker() {
-      showModalBottomSheet(
-        context: context,
-        builder: (BuildContext context) {
-          return EmojiPicker(
-              //onEmojiSelected: {
-              /*
-              (Category category, Emoji emoji) {
-              insertEmoji(emoji.emoji);
-            }
-            */
-              //},
-              );
-        },
-      );
-    }
-
     return ValueListenableBuilder<String>(
-        valueListenable: SharedValue.currentRoomId,
-        builder: (context, value, child) {
-          return Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Expanded(
+      valueListenable: SharedValue.currentRoomId,
+      builder: (context, value, child) {
+        return Column(
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Expanded(
                   child: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: TextField(
-                          contextMenuBuilder: (context, editableTextState) {
-                            final List<ContextMenuButtonItem> buttonItems =
-                                editableTextState.contextMenuButtonItems;
-
-                            buttonItems.insert(
-                              0,
-                              ContextMenuButtonItem(
-                                label: 'Add Quote', // I18n ?
-                                onPressed: () {
-                                  // Necessary to remove menu
-                                  ContextMenuController.removeAny();
-                                },
-                              ),
-                            );
-                            // buttonItems.add(PopupMenuDivider());
-
-                            return AdaptiveTextSelectionToolbar.buttonItems(
-                              anchors: editableTextState.contextMenuAnchors,
-                              buttonItems: buttonItems,
-                            );
-                          },
-                          enabled: SharedValue.currentRoomId.value.isNotEmpty,
-                          keyboardType: TextInputType.multiline,
-                          minLines: 1,
-                          maxLines: 3,
-                          autocorrect: true,
-                          controller: _controller,
-                          decoration: InputDecoration(
+                      child: Autocomplete<String>(
+                        optionsViewOpenDirection: OptionsViewOpenDirection.up,
+                        optionsBuilder: (TextEditingValue textEditingValue) {
+                          if (textEditingValue.text == '') {
+                            return const Iterable<String>.empty();
+                          }
+                          return _suggestions.where((String option) {
+                            return option.contains(textEditingValue.text.toLowerCase());
+                          });
+                        },
+                        onSelected: (String selection) {
+                          debugPrint('You just selected $selection');
+                        },
+                        fieldViewBuilder: (BuildContext context,
+                            TextEditingController fieldTextEditingController,
+                            FocusNode fieldFocusNode,
+                            VoidCallback onFieldSubmitted) {
+                          return TextField(
+                            controller: fieldTextEditingController,
+                            focusNode: fieldFocusNode,
+                            contextMenuBuilder: (context, editableTextState) {
+                              final List<ContextMenuButtonItem> buttonItems =
+                                  editableTextState.contextMenuButtonItems;
+                              buttonItems.insert(
+                                0,
+                                ContextMenuButtonItem(
+                                  label: 'Add Quote',
+                                  onPressed: () {
+                                    ContextMenuController.removeAny();
+                                  },
+                                ),
+                              );
+                              return AdaptiveTextSelectionToolbar.buttonItems(
+                                anchors: editableTextState.contextMenuAnchors,
+                                buttonItems: buttonItems,
+                              );
+                            },
+                            enabled: SharedValue.currentRoomId.value.isNotEmpty,
+                            keyboardType: TextInputType.multiline,
+                            minLines: 1,
+                            maxLines: 3,
+                            autocorrect: true,
+                            decoration: InputDecoration(
                               prefixIcon: IconButton(
-                                  onPressed: showEmojiPicker, icon: Icon(Icons.emoji_emotions)),
+                                onPressed: showEmojiPicker,
+                                icon: Icon(Icons.emoji_emotions),
+                              ),
                               suffixIcon: IconButton(
-                                onPressed: _controller.clear,
+                                onPressed: fieldTextEditingController.clear,
                                 icon: const Icon(Icons.clear),
                               ),
-                              border: const OutlineInputBorder())))),
-              FilledButton(
-                  child: Text("Send",
-                      style: TextStyle(color: Colors.white, fontSize: 18)), // TODO I18n !
+                              border: const OutlineInputBorder(),
+                            ),
+                          );
+                        },
+                      )),
+                ),
+                FilledButton(
+                  child: Text(
+                    "Send",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
                   onPressed: () {
                     if (SharedValue.currentRoomId.value.isNotEmpty) {
                       if (_controller.text.isNotEmpty && value.isNotEmpty) {
@@ -140,9 +129,13 @@ class MessagelineState extends State<Messageline> {
                         _controller.text = "";
                       }
                     }
-                  }),
-            ],
-          );
-        });
+                  },
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
   }
 }
