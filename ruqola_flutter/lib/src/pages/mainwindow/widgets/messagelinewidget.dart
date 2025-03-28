d72812be59aa746ff58b57bcf094d7704c16f092
Laurent Montel <laurent@kdab.com>
2025-03-28 09:26:31 +0100

    Add license

--- ruqola_flutter/lib/src/pages/mainwindow/widgets/messagelinewidget.dart
+++ ruqola_flutter/lib/src/pages/mainwindow/widgets/messagelinewidget.dart
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Laurent Montel <laurent.montel@kdab.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
 import 'package:flutter/material.dart';
 import 'package:libruqolacore/libruqolacore.dart';
 import 'package:ruqola_flutter/src/pages/mainwindow/widgets/sharedvalue.dart';
