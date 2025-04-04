7aad8f459b025224650c726b1d7c52e321dcd45a
Laurent Montel <laurent@kdab.com>
2025-04-04 13:30:55 +0200

    Fix implement ownuser

--- libruqolacore/lib/src/ownuser/ownuser.dart
+++ libruqolacore/lib/src/ownuser/ownuser.dart
@@ -17,7 +17,62 @@ class OwnUser {
   final String statusDefault = '';
   final String nickName = '';
   final List<String> roles = [];
-  ServicePassword mServicePassword = ServicePassword.defaultValues();
+  ServicePassword servicePassword = ServicePassword.defaultValues();
   OwnUserPreferences ownUserPreferences = OwnUserPreferences();
-  double mUtcOffset = 0.0;
+  double utcOffset = 0.0;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is OwnUser &&
+        other.userId == userId &&
+        other.userName == userName &&
+        other.status == status &&
+        other.email == email &&
+        other.statusText == statusText &&
+        other.name == name &&
+        other.avatarUrl == avatarUrl &&
+        other.statusDefault == statusDefault &&
+        other.nickName == nickName &&
+        other.servicePassword == servicePassword &&
+        other.roles == roles &&
+        other.ownUserPreferences == ownUserPreferences &&
+        other.utcOffset == utcOffset;
+  }
+
+  @override
+  int get hashCode {
+    final String userId = '';
+    final String userName = '';
+    final String status = '';
+    final String email = '';
+    final String statusText = '';
+    final String name = '';
+    final String avatarUrl = '';
+    final String statusDefault = '';
+    final String nickName = '';
+    final List<String> roles = [];
+    ServicePassword servicePassword = ServicePassword.defaultValues();
+    OwnUserPreferences ownUserPreferences = OwnUserPreferences();
+    double utcOffset = 0.0;
+
+    return userId.hashCode ^
+        userName.hashCode ^
+        status.hashCode ^
+        email.hashCode ^
+        statusText.hashCode ^
+        name.hashCode ^
+        avatarUrl.hashCode ^
+        nickName.hashCode ^
+        roles.hashCode ^
+        servicePassword.hashCode ^
+        ownUserPreferences.hashCode ^
+        utcOffset.hashCode ^
+        statusDefault.hashCode;
+  }
+
+  @override
+  String toString() {
+    return 'OwnUser(userId: $userId, userName: $userName, status: $status, email: $email, statusText: $statusText, name: $name, avatarUrl: $avatarUrl, nickName: $nickName, roles: $roles, servicePassword: $servicePassword, ownUserPreferences: $ownUserPreferences, utcOffset: $utcOffset, statusDefault: $statusDefault)';
+  }
 }
