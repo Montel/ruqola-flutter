252972a19f4bfb16aa3284d7d195444b519483a4
Laurent Montel <laurent@kdab.com>
2025-04-18 18:50:38 +0200

    Prepare rolesmodel

---
+++ libruqolacore/lib/src/roles/rolesmodel.dart
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Laurent Montel <laurent.montel@kdab.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+import 'package:flutter/material.dart';
+import 'package:libruqolacore/src/roles/roles.dart';
+
+class RolesModel with ChangeNotifier {
+  Roles roles = Roles();
+  // TODO
+}
