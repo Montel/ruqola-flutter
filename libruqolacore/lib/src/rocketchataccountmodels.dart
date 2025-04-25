c10524989be3ee77bee239d52df45cbb94f3b8fe
Laurent Montel <laurent@kdab.com>
2025-04-25 16:27:44 +0200

    Prepare to add filtering

--- libruqolacore/lib/src/rocketchataccountmodels.dart
+++ libruqolacore/lib/src/rocketchataccountmodels.dart
@@ -149,7 +149,7 @@ RoomModel::Section RoomModel::section(Room *r) const
   }
 
   Map<String, List<Room>> sortedRoomsWithType(
-      [RoomListSortOrder list = RoomListSortOrder.alphabetically]) {
+      [RoomListSortOrder list = RoomListSortOrder.alphabetically, String filter = ""]) {
     Map<String, List<Room>> groupedRooms = {};
 
     rooms.sort((a, b) => compare(a, b, list));
