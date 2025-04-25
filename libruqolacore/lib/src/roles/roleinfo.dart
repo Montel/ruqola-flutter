faa990553ab5aadb9482223bec3b661e879c5a8d
Laurent Montel <laurent@kdab.com>
2025-04-25 15:32:28 +0200

    Implement roleInfo

--- libruqolacore/lib/src/roles/roleinfo.dart
+++ libruqolacore/lib/src/roles/roleinfo.dart
@@ -5,5 +5,32 @@
  */
 
 class RoleInfo {
-  // TODO
+  String name = '';
+  String identifier = '';
+  String scope = '';
+  String description = '';
+  bool roleProtected = false;
+  bool mandatory2fa = false;
+
+  // factory RoleInfo.fromJson(Map<String, dynamic> json) {}
+
+  @override
+  bool operator ==(Object other) {
+    return other is RoleInfo &&
+        other.name == name &&
+        other.identifier == identifier &&
+        other.scope == scope &&
+        other.description == description &&
+        other.mandatory2fa == mandatory2fa &&
+        other.roleProtected == roleProtected;
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(name, identifier, scope, description, mandatory2fa, roleProtected);
+
+  @override
+  String toString() {
+    return 'RoleInfo(name: $name, identifier: $identifier, scope: $scope, description: $description, mandatory2fa: $mandatory2fa, roleProtected: $roleProtected)';
+  }
 }
