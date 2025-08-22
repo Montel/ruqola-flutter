4658afb6e3a7620df0d3e75f0aae1f59583114b4
Laurent Montel <laurent@kdab.com>
2025-08-22 15:20:49 +0200

    Use return switch

--- libddpapi/lib/src/notification/messagenotificationtype.dart
+++ libddpapi/lib/src/notification/messagenotificationtype.dart
@@ -19,27 +19,17 @@ enum NotificationType {
 }
 
 NotificationType convertStringToNotificationType(String msgType) {
-  if (msgType == 'ping') {
-    return NotificationType.ping;
-  } else if (msgType == 'connected') {
-    return NotificationType.connected;
-  } else if (msgType == 'error') {
-    return NotificationType.error;
-  } else if (msgType == 'added') {
-    return NotificationType.added;
-  } else if (msgType == 'changed') {
-    return NotificationType.changed;
-  } else if (msgType == 'removed') {
-    return NotificationType.removed;
-  } else if (msgType == 'nosub') {
-    return NotificationType.nosub;
-  } else if (msgType == 'ready') {
-    return NotificationType.ready;
-  } else if (msgType == 'updated') {
-    return NotificationType.updated;
-  } else if (msgType == 'result') {
-    return NotificationType.result;
-  } else {
-    return NotificationType.unknown;
-  }
+  return switch (msgType) {
+    'ping' => NotificationType.ping,
+    'connected' => NotificationType.connected,
+    'error' => NotificationType.error,
+    'added' => NotificationType.added,
+    'changed' => NotificationType.changed,
+    'removed' => NotificationType.removed,
+    'nosub' => NotificationType.nosub,
+    'ready' => NotificationType.ready,
+    'updated' => NotificationType.updated,
+    'result' => NotificationType.result,
+    _ => NotificationType.unknown,
+  };
 }
